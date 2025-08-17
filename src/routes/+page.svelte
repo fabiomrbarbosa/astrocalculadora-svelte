976cf7d0387012979f0780c0f97ca66e547d7cb6
Fábio M.R. Barbosa <fabio@fabiobarbosa.com>
2025-08-17 17:25:39 +0200

    Add timezone warning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -151,6 +151,26 @@
 				Criar Mapa
 			{/if}
 		</button>
+
+		<div role="alert" class="alert alert-warning alert-soft mt-4">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-6 w-6 shrink-0 stroke-current"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+				/>
+			</svg>
+			<span class="text-xs"
+				>De momento, esta calculadora apenas aplica os <strong>padrões actuais</strong> de fusos horários
+				e horários de Verão. Poderão ocorrer desvios, sobretudo em datas históricas.</span
+			>
+		</div>
 	</form>
 
 	{#if chartData.rawEphemeris}
