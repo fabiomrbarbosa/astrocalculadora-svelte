f8f4f321704c9edcf214acc4e67c9e17c6e2d66e
Fábio M.R. Barbosa <fabio@fabiobarbosa.com>
2025-10-14 18:04:55 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -156,7 +156,7 @@
 				<option selected value={'B'}>Alcabitius</option>
 				<option value={'R'}>Regiomontanus</option>
 				<option value={'P'}>Placidus</option>
-				<option value={'W'}>Signo Inteiro</option>
+				<option value={'W'}>Signos Inteiros</option>
 			</select>
 		</fieldset>
 
